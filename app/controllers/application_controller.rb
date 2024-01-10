039c4d2fc8efc52147e4e25f14397c154a982927
SAORI KODERA <tibezu.dash@gmail.com>
2024-01-10 13:19:11 +0900

    課題の修正をしました

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,17 +12,10 @@ private
     end
   end
 
-  # def require_loginexi
-  #   unless session[:user_id]
-
-  #     flash[:error] = “ログインしてください”
-  #     redirect_to login_path
-  #   end
-  # end
 
   def logged_in_user
     unless logged_in?
-      redirect_to task_path
+      redirect_to tasks_path
     end
   end
 end
